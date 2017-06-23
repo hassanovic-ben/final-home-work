ed674a1148aaf3f2b76a8f2042d21f951736fc94
Hassanb <mustafa-aittah>
2017-06-23 15:24:27 +0200

    cococo

--- src/main/java/be/benabdelali/BookStoreApplication.java
+++ src/main/java/be/benabdelali/BookStoreApplication.java
@@ -1,17 +1,11 @@
 package be.benabdelali;
 
 
-import be.benabdelali.model.Admin;
-import be.benabdelali.model.Client;
-import be.benabdelali.services.AdminService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 @EntityScan(basePackages = {"be.benabdelali.model"})
